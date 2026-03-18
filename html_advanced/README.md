bb7a31777fc756140e5d0139c3459cb0a3af17cc
ylabate <12196@holbertonstudents.com>
2026-03-18 22:40:51 +0100

    Feat(html): Add html_advanced pages and assets

---
+++ html_advanced/README.md
@@ -0,0 +1,10 @@
+# html advanced
+
+Ce dossier contient la structure HTML avancée pour le site web Techium.
+
+- `about.html`: La page "À propos" (About) du site Techium.
+- `article.html`: Un modèle de page d'article pour le site Techium.
+- `contact.html`: La page de contact du site Techium.
+- `index.html`: La page d'accueil (Homepage) du site Techium.
+- `latest_news.html`: La page des dernières actualités (Latest news) du site Techium.
+- `styleguide.html`: Le guide de style (Styleguide) du site Techium.
---
+++ html_advanced/next.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/sh
+
+filename="-index.html"
+
+last=0
+
+w3c="ignored"
+
+methode=""
+
+from="touch"
+
+finally() {
+	exit_status=$?
+
+	if [ $((exit_status)) -ne 0 ]; then
+		printf "w3c %s | %s\n" "$w3c" "$prev_file" >&2
+	elif [ "$methode" = "touched" ]; then
+		printf "w3c %s | %s %s\n"  "$w3c" "$current_file" "$methode"
+	else
+		printf "w3c %s | %s %s from %s\n"  "$w3c" "$current_file" "$methode" "$from"
+	fi
+	
+	exit $((exit_status))
+}
+
+trap finally EXIT
+
+while [ -f "$last$filename" ]; do
+	last=$((last + 1))
+done
+
+prev_file="$((last - 1))$filename"
+current_file="$last$filename"
+
+if [ "$last" -eq 0 ]; then
+	touch "$current_file"
+	methode="touched"
+
+else
+
+	if [ "${1:-}" = "-bypass" ]; then
+		w3c="bypassed"
+	else
+			if /usr/local/bin/htmlhint "$prev_file" > /dev/null 2>&1; then
+				w3c="passed"
+			else
+				if [ "$(head -n 1 "$prev_file")" = "placeholder" ]; then
+					echo placeholder
+				else
+					w3c="failed"
+					exit 1
+				fi
+			fi
+		fi
+		cp "$prev_file" "$current_file"
+		methode="copied"
+		from="$prev_file"
+	fi
